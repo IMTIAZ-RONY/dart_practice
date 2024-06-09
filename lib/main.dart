bd3e9781bc1a66bd30a192e52c8a90cb53ccb32a
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2024-06-10 00:33:51 +0600

    Dart & OOP practice

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  /// Sting Concatenation & Interpolation
+String a= "I";
+String b= "Love";
+String c= "My";
+String d= "Daughter";
+/// String literal
+String e ="I" "Love""My" "Daughter";
+
+///5th Step
+List <String>f=["I",'Love','My','Mother'];
+print(f.join());
+
+///1st Step
+print(a+b+c+d) ;
+///2nd Step
+print("$a$b$c$d");
+///3rd Step
+print("only${a+b+c+d}");
+///4th Step
+print(e);
+
+}
